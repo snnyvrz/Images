6126e28c4b2f2ab2736f57570237631d49a64e18
sinaniya <sinaniya@gmail.com>
2021-01-24 14:47:38 +0330

    add support for web

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,9 +20,12 @@ class App extends StatelessWidget {
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: () {
-            print('Hi there!');
+            setState(() {
+              counter += 1;
+            });
           },
         ),
+        body: Text('$counter'),
       ),
     );
   }
